7f493e7800f1abd6b879e6f6e91830aa62ec43d5
mohammedrashid <mohammed1rashid786@gmail.com>
2023-02-09 21:39:21 +0530

    functionStart

--- CardAppStoryboard/ViewController.swift
+++ CardAppStoryboard/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var uiImageView: UIImageView!
+    
+    @IBOutlet var stopButtonOutlet: UIButton!
+    
+    @IBOutlet var restartButtonOutlet: UIButton!
+    
+    @IBOutlet var rulesButtonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopButtonOutlet.layer.cornerRadius = 8
+        restartButtonOutlet.layer.cornerRadius = 8
+        rulesButtonOutlet.layer.cornerRadius = 8
+
     }
 
-
+    @IBAction func stopButton(_ sender: UIButton) {
+    }
+    
 }
 
